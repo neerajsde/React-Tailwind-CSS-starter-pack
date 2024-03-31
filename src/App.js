4135d7294994631d60de9f5f873a7feaa882bbdb
neerajsde <neerajprajapatise@gmail.com>
2024-03-31 23:11:58 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-red-500 font-bold text-center">
+      React Tailwind Starter Pack
+    </div>
+  );
+}
+
+export default App;
